e894d9aca5fc6e1bec3040512065890a80af0522
jecastrom <jcastro.de@outlook.com>
2022-01-05 19:55:41 +0100

    See README for solution

--- files_for_lab/SQL lab 8 solutions script.sql
+++ files_for_lab/SQL lab 8 solutions script.sql
@@ -1,5 +1,16 @@
 -- SQL Lab 8
 -- 1. Rank films by length
+SELECT
+    title,
+    `length`,
+    dense_rank() over(
+        ORDER BY
+            `length` DESC
+    ) AS ranking
+FROM
+    film
+WHERE
+    `length` NOT IN (0, '', ' ');
 -- 2. Rank films by length within the rating category
 -- 3. How many films are there for each of the categories in the category table?
 -- 4. Which actor has appeared in the most films?
