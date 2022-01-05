92cbfaa9c2600f7a07be75da0557fc8075b14be4
jecastrom <jcastro.de@outlook.com>
2022-01-05 21:05:36 +0100

    a

--- files_for_lab/SQL lab 8 solutions script.sql
+++ files_for_lab/SQL lab 8 solutions script.sql
@@ -27,6 +27,16 @@ WHERE
 ORDER BY
     3;
 -- 3. How many films are there for each of the categories in the category table?
+SELECT
+    a.`name` AS film_category,
+    count(b.film_id) AS number_of_films
+FROM
+    category a
+    INNER JOIN film_category b ON a.category_id = b.category_id
+GROUP BY
+    1
+ORDER BY
+    2 DESC;
 -- 4. Which actor has appeared in the most films?
 -- 5. Most active customer
 -- 6. Find which is the most rented film
