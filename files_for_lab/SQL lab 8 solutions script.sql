d00826ab06e4fa94802cfef9dcd349bceddecdf1
jecastrom <jcastro.de@outlook.com>
2022-01-05 20:30:09 +0100

    a

--- files_for_lab/SQL lab 8 solutions script.sql
+++ files_for_lab/SQL lab 8 solutions script.sql
@@ -12,6 +12,20 @@ FROM
 WHERE
     `length` NOT IN (0, '', ' ');
 -- 2. Rank films by length within the rating category
+SELECT
+    title,
+    `length`,
+    rating,
+    dense_rank() over(
+        ORDER BY
+            `length` DESC
+    ) AS ranking
+FROM
+    film
+WHERE
+    `length` NOT IN (0, '', ' ')
+ORDER BY
+    3;
 -- 3. How many films are there for each of the categories in the category table?
 -- 4. Which actor has appeared in the most films?
 -- 5. Most active customer
